19361846e4a9f5951e4a36a3d1e2815937895c53
ABHISHEK VISHNOI <139157548+abhishekxdev@users.noreply.github.com>
2025-07-17 14:08:55 +0530

    Replace header with positioned images

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,34 @@ function App() {
       
       {/* Header Section */}
       <div className="relative z-10 w-full">
-        {/* Header Image */}
-        <div className="flex justify-center px-6 lg:px-12 py-6">
-          <img 
-            src="https://res.cloudinary.com/doevp9obh/image/upload/v1752643836/Screenshot_2025-07-16_at_10.50.51_AM-removebg-preview_lpy8kn.png"
-            alt="Header"
-            className="h-auto max-w-full object-contain"
-          />
+        {/* Header Images */}
+        <div className="relative px-6 lg:px-12 py-6">
+          {/* Top Left Image */}
+          <div className="absolute top-6 left-6 lg:left-12">
+            <img 
+              src="https://res.cloudinary.com/doevp9obh/image/upload/v1752741437/Screenshot_2025-07-17_at_2.05.36_PM-removebg-preview_bd0iwm.png"
+              alt="Top Left"
+              className="h-auto max-w-full object-contain"
+            />
+          </div>
+          
+          {/* Top Right Image */}
+          <div className="absolute top-6 right-6 lg:right-12">
+            <img 
+              src="https://res.cloudinary.com/doevp9obh/image/upload/v1752741437/Screenshot_2025-07-17_at_2.05.51_PM-removebg-preview_ofmsw6.png"
+              alt="Top Right"
+              className="h-auto max-w-full object-contain"
+            />
+          </div>
+          
+          {/* Center Image */}
+          <div className="flex justify-center">
+            <img 
+              src="https://res.cloudinary.com/doevp9obh/image/upload/v1752741438/Screenshot_2025-07-17_at_2.05.59_PM-removebg-preview_jrcvnz.png"
+              alt="Center"
+              className="h-auto max-w-full object-contain"
+            />
+          </div>
         </div>
       </div>
       
